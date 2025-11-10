90aa508ff34b55d82793f08a6e9c2bf30347be62
Amirhossein <71593058+ahaf138396@users.noreply.github.com>
2025-11-10 21:00:18 +0330

    1404-08-19 | Test 8

--- crawler/storage/postgres_init.py
+++ crawler/storage/postgres_init.py
@@ -1,13 +1,20 @@
+import os
 from tortoise import Tortoise
 from loguru import logger
 
-DB_URL = "postgres://jooya:SuperSecurePass123@postgres:5432/jooyacrawler"
+DB_USER = os.getenv("POSTGRES_USER", "jooya")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "SuperSecurePass123")
+DB_NAME = os.getenv("POSTGRES_DB", "jooyacrawlerdb")
+DB_HOST = os.getenv("POSTGRES_HOST", "postgres")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+DB_URL = f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 async def init_postgres():
     logger.info("Initializing PostgreSQL and ORM models...")
     await Tortoise.init(
         db_url=DB_URL,
-        modules={"models": ["crawler.storage.models.queue_model"]}
+        modules={"models": ["crawler.storage.models.queue_model"]},
     )
     await Tortoise.generate_schemas(safe=True)
-    logger.success("✅ PostgreSQL tables created or verified.")
+    logger.info("PostgreSQL tables created or verified.")
