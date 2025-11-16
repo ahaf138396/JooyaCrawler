1316316ecb57a63d92ea9fa223007885b18c38bb
Amirhossein <71593058+ahaf138396@users.noreply.github.com>
2025-11-16 23:36:26 +0330

    1404-08-25 | Fix Queue Model By Replacing TextField With CharField For Unique Index Support

--- crawler/storage/models/queue_model.py
+++ crawler/storage/models/queue_model.py
@@ -1,10 +1,13 @@
-from tortoise import models, fields
+from tortoise import fields, models
 
 class CrawlQueue(models.Model):
     id = fields.IntField(pk=True)
-    url = fields.TextField(unique=True)   # جلوگیری از تکرار
-    status = fields.CharField(max_length=20, default="pending")
+
+    url = fields.CharField(max_length=2048, unique=True)
+
+    status = fields.CharField(max_length=20, default="pending")  # pending, processing, done
     created_at = fields.DatetimeField(auto_now_add=True)
 
     class Meta:
         table = "crawl_queue"
+        indexes = ("url",)
