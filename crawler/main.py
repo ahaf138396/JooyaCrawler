e0fbee2d9a575fe96cee5246f16e75724fea40ea
Amirhossein <71593058+ahaf138396@users.noreply.github.com>
2025-11-18 00:15:40 +0330

    Revert "Bug Fixing"
    This reverts commit a5d577644be1996bc890f67f1b19abe2002683d6.

--- crawler/main.py
+++ crawler/main.py
@@ -57,7 +57,7 @@ async def main() -> None:
 
     # ---- Seed URLs ----
     seed_urls = [
-        "https://fa.wikipedia.org/wiki/%D8%B5%D9%81%D8%AD%D9%87%D9%94_%D8%A7%D8%B5%D9%84%DB%8C",
+        "https://example.com",
     ]
     scheduler = Scheduler(queue, seed_urls, interval_seconds=10)
 
